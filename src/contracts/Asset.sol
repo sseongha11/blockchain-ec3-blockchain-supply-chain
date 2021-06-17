205d829e56c4f7b7378949ece616ea0d5cd8324d
seongha <sseongha11@gmail.com>
2021-06-17 13:16:57 +0100

    modifled Asset.sol

--- src/contracts/Asset.sol
+++ src/contracts/Asset.sol
@@ -1,15 +1,36 @@
+// Any programming language provides the facility to comment code and so does Solidity.
+// With the help of the pragma directive, you can choose the compiler version and target your code accordingly.
 pragma solidity ^0.5.0;
 
+// we can define contracts, libraries, and interfaces at the global or top level.
+// we will simply define one contract (Asset)
 contract Asset {
-  string public name;
-  address public custodian;
-  STATUSES public status;
+    
+  /* Variables in programming refer to storage location that can contain values. 
+  These values can be changed during runtime.*/    
 
+  // Asset name such as steel structures and gas turbine
+  string public name;
+
+  /* Accounts are the main building blocks for the Ethereum ecosystem. It is an interaction between accounts that Ethereum wants to store as transactions in its ledger.
+  There are two types of accounts available in Ethereum—externally owned accounts and contract accounts.
+  Contract accounts are very similar to externally owned accounts. */
+  address public custodian; 
+  
+  STATUSES public status;
+  
+  /* The enum keyword is used to declare enumerations. 
+  Enumerations help in declaring a custom user-defined data type in Solidity */
+  
+  // enumeration declaraion!
   enum STATUSES {
     CREATED,
     SENT,
     RECEIVED
   }
+
+  /* Solidity supports events. Events in Solidity are just like events in other programming languages. 
+  Events are fired from contracts such that anybody interested in them can trap/catch them and execute code in response. */
 
   event Action(
     string name,
@@ -25,15 +46,18 @@ contract Asset {
     // Make deployer custodian
     custodian = msg.sender;
 
-    // Update status to "CREATED"
+    // using enum - Update status to "CREATED"
     status = STATUSES.CREATED;
 
     // Log history
     emit Action("CREATE", msg.sender, msg.sender, now);
   }
+  
+  /* Functions are the heart of Ethereum and Solidity. (public, internal, private and external)
+  Ethereum maintains the current state of state variables and executes transaction to change values in state variables */
 
   function send(address _to) public {
-    // Must be custodian to send
+    // Must be custodian to send - cehck the conditiona
     require(msg.sender == custodian);
 
     // Cannot send to self
